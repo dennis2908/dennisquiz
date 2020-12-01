0afff6c4ca861ec09deb017a1b5a08807b88645e
dennis M <manullang_d@yahoo.com>
2020-12-01 11:07:53 +0700

    dm

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -20,5 +20,51 @@ body{
 <h4><span class="badge badge-success p-1">Benar </span> : <span class="badge badge-success p-1">{{session::get('score')}}</span></h4>
 <h4><span class="badge badge-danger p-1">Salah </span> : <span class="badge badge-danger p-1">{{session::get('incorrect')}}</span></h4>
 </div>
+<script>
+$(function() {
+    // WARNING: Extremely hacky code ahead. jQuery mobile automatically
+    // sets the current "page" height on page resize. We need to unbind the
+    // resize function ONLY and reset all pages back to auto min-height.
+    // This is specific to jquery 1.8
+
+    // First reset all pages to normal
+    $('[data-role="page"]').css('min-height', 'auto');
+
+    // Is this the function we want to unbind?
+    var check = function(func) {
+        var f = func.toLocaleString ? func.toLocaleString() : func.toString();
+        // func.name will catch unminified jquery mobile. otherwise see if
+        // the function body contains two very suspect strings
+        if(func.name === 'resetActivePageHeight' || (f.indexOf('padding-top') > -1 && f.indexOf('min-height'))) {
+            return true;
+        }
+    };
+
+    // First try to unbind the document pageshow event
+    try {
+        // This is a hack in jquery 1.8 to get events bound to a specific node
+        var dHandlers = $._data(document).events.pageshow;
+
+        for(x = 0; x < dHandlers.length; x++) {
+            if(check(dHandlers[x].handler)) {
+                $(document).unbind('pageshow', dHandlers[x]);
+                break;
+            }
+        }
+    } catch(e) {}
+
+    // Then try to unbind the window handler
+    try {
+        var wHandlers = $._data(window).events.throttledresize;
+
+        for(x = 0; x < wHandlers.length; x++) {
+            if(check(wHandlers[x].handler)) {
+                $(window).unbind('throttledresize', wHandlers[x]);
+                break;
+            }
+        }
+    } catch(e) {}
+});
+</script>
 </body>
 </html> 
