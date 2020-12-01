c5b58016463b3ba5fcf2fbf913ef62ff67f5117d
dennis M <manullang_d@yahoo.com>
2020-12-01 10:57:20 +0700

    dm

---
+++ database/answers.sql
@@ -0,0 +1,12 @@
+INSERT INTO answers (id, type, answer, created_at, updated_at) VALUES
+(1, 'Short response', '3220', NULL, NULL),
+(2, 'Short response', '300', NULL, NULL),
+(3, 'Short response', '1000', NULL, NULL),
+(4, 'Short response', '5000', NULL, NULL),
+(5, 'Short response', '2000', NULL, NULL),
+(6, 'Short response', '9000', NULL, NULL),
+(7, 'Short response', '100', NULL, NULL),
+(8, 'matching pair', '2+4', NULL, NULL),
+(9, 'Multiple choice single answer', '20+20', NULL, NULL),
+(10, 'Multiple choice two correct', '20+10,5+25', NULL, NULL),
+(11, 'Short response', '4000', NULL, NULL);
