26cf1a6c0b377198dce95544abeb1eb71a52cc8f
dennis M <manullang_d@yahoo.com>
2020-12-01 15:35:02 +0700

    dmdmdm

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -60,7 +60,7 @@ class HomeController extends Controller
 			else{
 				$urut = ($this->length) - count($array_rand);
 			}
-			return view('result',compact('urut'));
+			return view('ques11',compact('urut'));
 		}
 		
 		
